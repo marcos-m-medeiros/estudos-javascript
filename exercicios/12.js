b5cc764d2db0e4b416cc4f06f3743014f24d6b10
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-09-27 17:50:19 -0300

    Exercício 12
    O programa usa recursão para calcular o fatorial de um número. Se o número for menor que 0, ele retorna -1 para indicar um valor inválido. Se o número for igual a 0, ele retorna 1, porque o fatorial de 0 é definido como 1. Caso contrário, ele calcula o fatorial usando recursão, chamando a função `fatorial` com `num - 1` e multiplicando o resultado por `num`. Isso é feito repetidamente até que num seja reduzido a 0. O resultado é impresso no console, e no caso de `fatorial(5)`, o resultado é 120.

--- exercicios/12.js
+++ exercicios/12.js
@@ -1,13 +1,19 @@
-// Faça um algoritmo que calcule o fatorial de um número.
-
+// Esta é a definição da função 'fatorial' que aceita um parâmetro 'num'.
 function fatorial(num) {
+    // Primeira verificação: Se 'num' for menor que 0, retorna -1, indicando um valor inválido.
     if (num < 0) {
-        return -1
-    } else if (num == 0) {
-        return 1
-    } else {
-        return (num * fatorial(num - 1))
+        return -1;
+    }
+    // Segunda verificação: Se 'num' for igual a 0, retorna 1, pois o fatorial de 0 é definido como 1.
+    else if (num == 0) {
+        return 1;
+    }
+    // Terceira verificação: Caso contrário, calcula o fatorial de 'num' usando recursão.
+    else {
+        // A função chama a si mesma com 'num - 1' e multiplica o resultado por 'num', repetindo até que 'num' seja 0.
+        return (num * fatorial(num - 1));
     }
 }
 
-console.log(fatorial(5))
+// Chama a função 'fatorial' com o valor 5 como argumento e imprime o resultado no console.
+console.log(fatorial(5)); // Isso imprimirá o fatorial de 5, que é 120.
