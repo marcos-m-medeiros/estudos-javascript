1c032eb09f616c14cf33b044dcf067930c46121b
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-09-27 17:52:28 -0300

    Exercício 13
    O código usa a função `getDay()` para obter o dia da semana atual a partir de um objeto Date e, em seguida, executa um bloco `switch` com base nesse valor. Dependendo do dia da semana, ele exibe uma mensagem diferente no console, indicando se é um dia útil ou um fim de semana.

--- exercicios/13.js
+++ exercicios/13.js
@@ -1,40 +1,40 @@
-// Crie um programa que exibe se um dia é dia útil, fim de semana ou dia inválido dado o número referente ao
-// dia. Considere que domingo é o dia 1 e sábado é o dia 7. Utilize a estrutura Switch.
+// Cria um objeto Date que representa a data e hora atuais.
+let today = new Date();
 
-let today = new Date()
-let weekday = today.getDay() + 1
+// Obtém o dia da semana atual (um número entre 0 e 6, onde 0 representa Domingo, 1 representa Segunda-feira, etc.).
+let weekday = today.getDay() + 1; // Adiciona 1 para fazer a correspondência com o seu switch.
 
-switch(weekday) {
+// Inicia um bloco switch com base no valor de 'weekday'.
+switch (weekday) {
     case 1:
-        console.log('Domingo.')
-        console.log('Fim de semana.')
-        break
+        console.log('Domingo.');
+        console.log('Fim de semana.');
+        break;
     case 2:
-        console.log('Segunda-feira.')
-        console.log('Dia útil.')
-        break
+        console.log('Segunda-feira.');
+        console.log('Dia útil.');
+        break;
     case 3:
-        console.log('Terça-feira.')
-        console.log('Dia útil.')
-
-        break
+        console.log('Terça-feira.');
+        console.log('Dia útil.');
+        break;
     case 4:
-        console.log('Quarta-feira.')
-        console.log('Dia útil.')
+        console.log('Quarta-feira.');
+        console.log('Dia útil.');
+        break;
     case 5:
-        console.log('Quinta-feira.')
-        console.log('Dia útil.')
-        break
+        console.log('Quinta-feira.');
+        console.log('Dia útil.');
+        break;
     case 6:
-        console.log('Sexta-feira.')
-        console.log('Dia útil.')
-    
-        break
+        console.log('Sexta-feira.');
+        console.log('Dia útil.');
+        break;
     case 7:
-        console.log('Sábado.')
-        console.log('Fim de semana.')
-        break
+        console.log('Sábado.');
+        console.log('Fim de semana.');
+        break;
     default:
-        console.log('Dia inválido.')
-        break
+        console.log('Dia inválido.');
+        break;
 }
