4f099eaebd01a75f91bdade7d1624074b54d46a2
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-09-27 17:44:34 -0300

    Exercício 1

--- exercicios/1.js
+++ exercicios/1.js
@@ -1,13 +1,18 @@
-let n1
-let n2
-let res
+let n1; // Declaração de uma variável n1 (será usada para armazenar o primeiro número).
+let n2; // Declaração de uma variável n2 (será usada para armazenar o segundo número).
+let res; // Declaração de uma variável res (não está sendo utilizada neste código).
 
-function calc(n1, n2){
-    const sum = n1 + n2
-    const sub = n1 - n2
-    const mul = n1 * n2
-    const div = n1 / n2
-    console.log(`Sum: ${sum} Subtraction: ${sub} Multiplication: ${mul} Divided: ${div}`)
+// Esta é a definição de uma função chamada 'calc' que aceita dois parâmetros, 'n1' e 'n2'.
+function calc(n1, n2) {
+    // Dentro da função, quatro variáveis locais (sum, sub, mul e div) são criadas para armazenar resultados de operações.
+    const sum = n1 + n2; // Calcula a soma de n1 e n2.
+    const sub = n1 - n2; // Calcula a subtração de n1 por n2.
+    const mul = n1 * n2; // Calcula a multiplicação de n1 por n2.
+    const div = n1 / n2; // Calcula a divisão de n1 por n2.
+
+    // Exibe no console uma mensagem que inclui os resultados das operações.
+    console.log(`Sum: ${sum} Subtraction: ${sub} Multiplication: ${mul} Divided: ${div}`);
 }
 
-calc(10, 10)
+// Chama a função 'calc' com os valores 10 e 10 como argumentos.
+calc(10, 10);
