932e6ae0c5d95fd5ed48eccbe1f6c452e20cb77e
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-09-27 17:47:07 -0300

    Exercício 11
    No código, a função `bissexto` primeiro verifica se o ano não é divisível por 4, depois verifica se não é divisível por 100 e, por fim, verifica se é divisível por 400. Se qualquer uma dessas condições for verdadeira, o ano é considerado bissexto, e a função imprime "true" no console. Caso contrário, ela imprime "false". No caso do ano 5000, a função imprimirá "true" no console porque é divisível por 400 (mesmo que também seja divisível por 4 e por 100). Note que a função poderia ser simplificada para verificar apenas se o ano é divisível por 4 e, ao mesmo tempo, não é divisível por 100 ou é divisível por 400.

--- exercicios/11.js
+++ exercicios/11.js
@@ -1,12 +1,22 @@
+// Esta é a definição de uma função chamada 'bissexto' que aceita um parâmetro 'ano'.
 function bissexto(ano) {
+    // A primeira condição verifica se o ano não é divisível por 4.
     if (ano % 4 !== 0) {
-        console.log(true)
-    } else if (ano % 100 != 0) {
-        console.log(true)
-    } else if (ano % 400 == 0) {
-        console.log(true)
-    } else {
-        console.log(false)
+        console.log(true); // Se a condição for verdadeira, exibe 'true' no console.
+    } 
+    // A segunda condição verifica se o ano não é divisível por 100.
+    else if (ano % 100 != 0) {
+        console.log(true); // Se a condição for verdadeira, exibe 'true' no console.
+    } 
+    // A terceira condição verifica se o ano é divisível por 400.
+    else if (ano % 400 == 0) {
+        console.log(true); // Se a condição for verdadeira, exibe 'true' no console.
+    } 
+    // Se nenhuma das condições acima for atendida, significa que o ano não é bissexto.
+    else {
+        console.log(false); // Nesse caso, exibe 'false' no console.
     }
 }
-bissexto(5000)
+
+// Chama a função 'bissexto' com o valor 5000 como argumento.
+bissexto(5000);
