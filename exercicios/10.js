e48852913ecd02292f516f047df3ec92c76c1e08
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-09-27 17:45:16 -0300

    Exercício 10

--- exercicios/10.js
+++ exercicios/10.js
@@ -1,9 +1,14 @@
+// Esta é a definição de uma função chamada 'div' que aceita um parâmetro 'num'.
 function div(num) {
+    // Dentro da função, verifica se o valor de 'num' é divisível por 3 (ou seja, se o resto da divisão de 'num' por 3 é igual a 0).
     if (num % 3 == 0) {
-        console.log(true)
+        // Se o valor de 'num' for divisível por 3, a condição é verdadeira, então o código dentro deste bloco é executado.
+        console.log(true); // Exibe 'true' no console.
     } else {
-        console.log(false)
+        // Se o valor de 'num' não for divisível por 3, a condição é falsa, então o código dentro deste bloco é executado.
+        console.log(false); // Exibe 'false' no console.
     }
 }
 
-div(18)
+// Chama a função 'div' com o valor 18 como argumento.
+div(18);
