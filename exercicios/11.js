76906f9a5e132eb65d78a56031d6fa645e87a85b
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-09-27 17:48:04 -0300

    Exercício 11

--- exercicios/11.js
+++ exercicios/11.js
@@ -3,15 +3,15 @@ function bissexto(ano) {
     // A primeira condição verifica se o ano não é divisível por 4.
     if (ano % 4 !== 0) {
         console.log(true); // Se a condição for verdadeira, exibe 'true' no console.
-    } 
+    }
     // A segunda condição verifica se o ano não é divisível por 100.
     else if (ano % 100 != 0) {
         console.log(true); // Se a condição for verdadeira, exibe 'true' no console.
-    } 
+    }
     // A terceira condição verifica se o ano é divisível por 400.
     else if (ano % 400 == 0) {
         console.log(true); // Se a condição for verdadeira, exibe 'true' no console.
-    } 
+    }
     // Se nenhuma das condições acima for atendida, significa que o ano não é bissexto.
     else {
         console.log(false); // Nesse caso, exibe 'false' no console.
