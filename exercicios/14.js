359395e6821cb42c6fea66aa012f8c8caf7a4007
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-09-27 17:54:35 -0300

    Exercício 14
    O programa usa um `switch` para comparar a fruta passada como argumento, independentemente das letras maiúsculas ou minúsculas, com três casos: 'maçã', 'kiwi' e 'melancia'. Se a fruta corresponder a um desses casos, ele exibirá uma mensagem relacionada à fruta correspondente. Se não houver correspondência, ele exibirá uma mensagem padrão informando que o programa só testa maçã, kiwi e melancia. Neste caso, quando você chama a função com 'MeLANCia' (com diferentes combinações de letras maiúsculas/minúsculas), ele corresponderá a 'melancia' e exibirá a mensagem "Aqui está, são 3 reais o quilo." no console.

--- exercicios/14.js
+++ exercicios/14.js
@@ -1,23 +1,21 @@
-/* Crie uma estrutura condicional switch que receba uma string com o nome de uma fruta e que possua três
-casos: Caso maçã, retorne no console: “Não vendemos esta fruta aqui”. Caso kiwi, retorne: “Estamos com
-escassez de kiwis”. Caso melancia, retorne: “Aqui está, são 3 reais o quilo”. Teste com estas três opções .Crie
-também um default, que retornará uma mensagem de erro no console. */
-
+// Define uma função chamada 'fruit' que aceita um parâmetro chamado 'fruit'.
 let fruit = (fruit) => {
+    // O switch é usado para testar diferentes valores da variável 'fruit' após converter para minúsculas com 'toLowerCase()'.
     switch (fruit.toLowerCase()) {
         case 'maçã':
-            console.log('Não vendemos essa fruta aqui.')
-            break
+            console.log('Não vendemos essa fruta aqui.');
+            break;
         case 'kiwi':
-            console.log('Estamos em escassez de kiwis.')
-            break
+            console.log('Estamos em escassez de kiwis.');
+            break;
         case 'melancia':
-            console.log('Aqui está, são 3 reais o quilo.')
-            break
+            console.log('Aqui está, são 3 reais o quilo.');
+            break;
         default:
-            console.log('O programa só testa maçã, kiwi e melancia.')
-            break
+            console.log('O programa só testa maçã, kiwi e melancia.');
+            break;
     }
 }
 
-fruit('MeLANCia')
+// Chama a função 'fruit' com a string 'MeLANCia' como argumento.
+fruit('MeLANCia');
