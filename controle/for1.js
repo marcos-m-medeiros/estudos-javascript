1c91721dc81fb3a32dccbd318f194112243f51ea
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-09-27 22:53:58 -0300

    for1
    O primeiro loop `for` começa com `i` igual a 1 e aumenta até que `i` seja menor ou igual a 10. A cada iteração, ele imprime o valor de i.
    
    Em seguida, um array chamado "notas" é declarado, contendo valores numéricos que representam notas.
    
    O segundo loop for percorre o array "notas". Ele começa com `i` igual a 0 e continua até que `i` seja menor que o comprimento do array "notas". Em cada iteração, ele imprime o valor do elemento do array correspondente à posição atual de `i`, exibindo as notas.
    
    Portanto, o código gera uma saída que lista os números de 1 a 10 e, em seguida, imprime as notas contidas no array "notas".

---
+++ controle/for1.js
@@ -0,0 +1,13 @@
+// Um loop "for" que começa com i=1, executa enquanto i for menor ou igual a 10 e incrementa i em cada iteração.
+for (let i = 1; i <= 10; i++) {
+    console.log(`contador = ${i}`); // Imprime o valor atual de i.
+}
+
+// Um array chamado "notas" com valores numéricos.
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7];
+
+// Um loop "for" que percorre o array "notas" usando a variável "i" como índice.
+// Ele começa com i=0, executa enquanto i for menor do que o tamanho do array "notas", e incrementa i em cada iteração.
+for (let i = 0; i < notas.length; i++) {
+    console.log(`Nota = ${notas[i]}`); // Imprime o valor da nota no índice atual de "i".
+}
