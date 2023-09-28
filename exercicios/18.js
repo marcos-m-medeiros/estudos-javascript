7d902162a0f7ac5185568288bbc53b0a0d9d2311
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-09-28 00:40:22 -0300

    Exercício 18
    Este programa é uma maneira simples de converter números inteiros em seus equivalentes por extenso dentro do intervalo especificado. É uma implementação direta usando uma instrução `switch` para mapear cada número para sua representação textual correspondente e lidar com números fora do intervalo.

--- exercicios/18.js
+++ exercicios/18.js
@@ -1,47 +1,45 @@
-/*
-Faça um programa que leia um número entre 0 e 10, e escreva este número por extenso. Use o comando
-switch. Crie um case default que escreva ‘Número fora do intervalo.’
- */
-
+// Defina uma função chamada numExtenso que recebe um número inteiro como argumento.
 let numExtenso = (numInt) => {
+    // Use uma instrução switch para verificar o valor do número inteiro.
     switch (numInt) {
         case 0:
-            console.log('Zero')
-            break
+            console.log('Zero'); // Se for 0, imprima "Zero".
+            break;
         case 1:
-            console.log('Um')
-            break
+            console.log('Um'); // Se for 1, imprima "Um".
+            break;
         case 2:
-            console.log('Dois')
-            break
+            console.log('Dois'); // Se for 2, imprima "Dois".
+            break;
         case 3:
-            console.log('Três')
-            break
+            console.log('Três'); // Se for 3, imprima "Três".
+            break;
         case 4:
-            console.log('Quatro')
-            break
+            console.log('Quatro'); // Se for 4, imprima "Quatro".
+            break;
         case 5:
-            console.log('Cinco')
-            break
+            console.log('Cinco'); // Se for 5, imprima "Cinco".
+            break;
         case 6:
-            console.log('Seis')
-            break
+            console.log('Seis'); // Se for 6, imprima "Seis".
+            break;
         case 7:
-            console.log('Sete')
-            break
+            console.log('Sete'); // Se for 7, imprima "Sete".
+            break;
         case 8:
-            console.log('Oito')
-            break
+            console.log('Oito'); // Se for 8, imprima "Oito".
+            break;
         case 9:
-            console.log('Nove')
-            break
+            console.log('Nove'); // Se for 9, imprima "Nove".
+            break;
         case 10:
-            console.log('Dez')
-            break
+            console.log('Dez'); // Se for 10, imprima "Dez".
+            break;
         default:
-            console.log('Número fora do intervalo 0 - 10')
-            break
+            console.log('Número fora do intervalo 0 - 10'); // Se for um número fora do intervalo, imprima uma mensagem de erro.
+            break;
     }
 }
 
-numExtenso(0)
+// Chame a função numExtenso com um número de exemplo (0) para ver a conversão por extenso.
+numExtenso(0);
