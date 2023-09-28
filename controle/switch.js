fecb06c200a10d8d54017ce104807f173a8675d8
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-09-28 01:37:32 -0300

    switch
    Este programa é uma maneira eficaz de classificar notas em diferentes categorias com base nos intervalos numéricos especificados, usando a estrutura de controle `switch`. Ele arredonda a nota para o número inteiro mais próximo usando `Math.floor` antes de avaliá-la nos casos do `switch`, garantindo que a lógica funcione mesmo se a nota for um número decimal.

--- controle/switch.js
+++ controle/switch.js
@@ -1,31 +1,43 @@
+// Define uma função anônima chamada 'imprimirResultado' que aceita um parâmetro 'nota'.
 const imprimirResultado = function (nota) {
+  // Utiliza a função Math.floor para arredondar a nota para o número inteiro mais próximo.
   switch (Math.floor(nota)) {
+    // Caso a nota seja 10 ou 9, imprime "Quadro de Honra".
     case 10:
     case 9:
       console.log("Quadro de Honra.");
       break;
+
+    // Caso a nota seja 8 ou 7, imprime "Aprovado".
     case 8:
     case 7:
-      console.log("Aprovavado.");
+      console.log("Aprovado.");
       break;
+
+    // Caso a nota seja 6, 5 ou 4, imprime "Recuperação".
     case 6:
     case 5:
     case 4:
       console.log("Recuperação.");
       break;
+
+    // Caso a nota seja 3, 2 ou 1, imprime "Reprovado".
     case 3:
     case 2:
     case 1:
       console.log("Reprovado.");
       break;
+
+    // Se a nota não se encaixar em nenhum dos casos anteriores, imprime "Nota Inválida".
     default:
       console.log("Nota Inválida.");
   }
 };
 
-imprimirResultado(10);
-imprimirResultado(8.9);
-imprimirResultado(6.55);
-imprimirResultado(2.3);
-imprimirResultado(-1);
-imprimirResultado(11);
+// Chama a função 'imprimirResultado' com diferentes valores de nota como argumentos.
+imprimirResultado(10); // Exemplo 1: "Quadro de Honra" será impresso no console.
+imprimirResultado(8.9); // Exemplo 2: "Aprovado" será impresso no console.
+imprimirResultado(6.55); // Exemplo 3: "Recuperação" será impresso no console.
+imprimirResultado(2.3); // Exemplo 4: "Reprovado" será impresso no console.
+imprimirResultado(-1); // Exemplo 5: "Nota Inválida" será impresso no console.
+imprimirResultado(11); // Exemplo 6: "Nota Inválida" será impresso no console.
