846dd3c297115c8165f6d55bec61fa9f61209460
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-09-28 00:48:45 -0300

    Exercício 6
    Este programa é uma calculadora simples que permite calcular juros simples e juros compostos com base nos valores de entrada e, em seguida, formata os resultados em moeda brasileira (BRL) usando o objeto `Intl.NumberFormat`. Os resultados calculados são exibidos no console.

--- exercicios/6.js
+++ exercicios/6.js
@@ -1,19 +1,27 @@
+// Define uma função chamada jurosSimples que calcula juros simples com base no capital inicial, taxa de juros e tempo de aplicação.
 function jurosSimples(capitalInicial, taxaJuros, tempoAplicacao) {
-    const juros = capitalInicial * taxaJuros * tempoAplicacao
-    return juros
+  const juros = capitalInicial * taxaJuros * tempoAplicacao; // Calcula os juros simples.
+  return juros; // Retorna o valor dos juros.
 }
 
-console.log(new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL'
-}).format(jurosSimples(1200, 0.02, 15)))
+// Chama a função jurosSimples com valores de exemplo (capital, taxa de juros e tempo) e formata o resultado em moeda brasileira.
+console.log(
+  new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(jurosSimples(1200, 0.02, 15))
+);
 
+// Define uma função chamada jurosCompostos que calcula juros compostos com base no capital inicial, taxa de juros e tempo de investimento.
 function jurosCompostos(capital, taxaJuros, tempoInvestido) {
-    const resultado = capital * Math.pow((1 + taxaJuros), tempoInvestido)
-    return resultado
+  const resultado = capital * Math.pow(1 + taxaJuros, tempoInvestido); // Calcula o resultado dos juros compostos.
+  return resultado; // Retorna o resultado dos juros compostos.
 }
 
-console.log(new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL'
-}).format(jurosCompostos(2000, 0.02, 6)))
+// Chama a função jurosCompostos com valores de exemplo (capital, taxa de juros e tempo) e formata o resultado em moeda brasileira.
+console.log(
+  new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(jurosCompostos(2000, 0.02, 6))
+);
