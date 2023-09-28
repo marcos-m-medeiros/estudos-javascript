a49d9d255e4360b52d1fb85666ead662487e571a
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-09-28 00:43:20 -0300

    Exercício 17
    Este programa permite que o usuário selecione um plano de trabalho (A, B ou C) e calcula o novo salário com base na porcentagem do plano selecionado. Ele formata o novo salário em moeda brasileira e lida com casos em que nenhum plano é selecionado ou um plano inválido é inserido. O exemplo dado chama a função com o plano 'c' para calcular o aumento salarial correspondente e exibir o novo salário formatado.

--- exercicios/17.js
+++ exercicios/17.js
@@ -1,51 +1,59 @@
-/*
-Um funcionário irá receber um aumento de acordo com o seu plano de
-trabalho, de acordo com a tabela abaixo:
-Plano Aumento
-A 10%
-B 15%
-C 20%
-Faça uma função que leia o plano de trabalho e o salário atual de um funcionário e calcula e imprime o seu
-novo salário. Use a estrutura switch e faça um caso default que indique que o plano é inválido.
-*/
+// Defina um salário inicial.
+let salario = 1000;
 
-let salario = 1000
-
+// Defina os planos de trabalho como porcentagens em um objeto.
 const plano = {
-    a: 10,
-    b: 15,
-    c: 20
-}
+  a: 10,
+  b: 15,
+  c: 20,
+};
 
+// Crie um objeto de formatação de número para formatar valores em moeda brasileira (BRL).
 let brlBR = Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-})
+  style: "currency",
+  currency: "BRL",
+});
 
+// Crie uma função chamada aumento que calcula o novo salário com base no plano selecionado.
 let aumento = (novoSalario) => {
+  console.log(
+    "Você pode apenas escolher um plano de trabalho. A = 10%, B = 15%, C = 20%."
+  );
 
-    console.log('Você pode apenas escolher um plano de trabalho. A = 10%, B = 15%, C = 20%.')
+  // Use uma instrução switch para verificar o plano de trabalho selecionado.
+  switch (novoSalario.toLowerCase()) {
+    case "a":
+      novoSalario = (plano.a / 100) * salario;
+      console.log(
+        `Você selecionou o PLANO A: 10%. \n\nSeu novo salário: ${brlBR.format(
+          novoSalario + salario
+        )}.`
+      );
+      break;
+    case "b":
+      novoSalario = (plano.b / 100) * salario;
+      console.log(
+        `Você selecionou o PLANO B: 15%. \n\nSeu novo salário: ${brlBR.format(
+          novoSalario + salario
+        )}.`
+      );
+      break;
+    case "c":
+      novoSalario = (plano.c / 100) * salario;
+      console.log(
+        `Você selecionou o PLANO C: 20%. \n\nSeu novo salário: ${brlBR.format(
+          novoSalario + salario
+        )}.`
+      );
+      break;
+    default:
+      if (novoSalario == "") {
+        console.log("Você não digitou o seu plano de trabalho.");
+      } else {
+        console.log("Plano inválido. Você digitou: " + novoSalario);
+      }
+  }
+};
 
-    switch (novoSalario.toLowerCase()) {
-        case 'a':
-            novoSalario = (plano.a / 100) * salario
-            console.log(`Você seleicou o PLANO A: 10%. \n\nSeu novo salário: ${brlBR.format(novoSalario + salario)}.`)
-            break
-        case 'b':
-            novoSalario = (plano.b / 100) * salario
-            console.log(`Você selecionou o PLANO B: 15%. \n\nSeu novo salário: ${brlBR.format(novoSalario + salario)}.`)
-            break
-        case 'c':
-            novoSalario = (plano.c / 100) * salario
-            console.log(`Você selecionou o PLANO C: 20%. \n\nSeu novo salário: ${brlBR.format(novoSalario + salario)}.`)
-            break
-        default:
-            if (novoSalario == '') {
-                console.log('Você não digitou o seu plano de trabalho.')
-            } else {
-                console.log('Plano inválido. Você digitou: ' + novoSalario)
-            }
-    }
-}
-
-aumento('c')
+// Chame a função aumento com um plano de exemplo ('c') para calcular o aumento salarial e imprimir o novo salário.
+aumento("c");
