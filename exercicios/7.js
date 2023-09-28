9d2ff3d11b1c0a23ebdde34b0e5ceca3ccb9bb9a
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-09-28 00:47:12 -0300

    Exercício 7
    Este programa calcula as raízes de uma equação quadrática e lida com casos em que não há raízes reais (quando o delta é negativo). Ele imprime as raízes no console quando elas existem. Note que, neste código, você está usando `console.log(bhaskara(1, -1, -6))`, mas a função bhaskara não retorna um valor explícito, então a saída será undefined. Para apenas calcular e mostrar as raízes no console, você pode remover o console.log externo.

--- exercicios/7.js
+++ exercicios/7.js
@@ -1,14 +1,21 @@
+// Defina uma função chamada bhaskara que calcula as raízes de uma equação quadrática.
 function bhaskara(a, b, c) {
-    const delta = b * b - 4 * a * c
+  // Calcule o valor do delta usando a fórmula delta = b^2 - 4ac.
+  const delta = b * b - 4 * a * c;
 
-    if (delta < 0) {
-        console.log('Sem raízes reais')
-    } else {
-        const x1 = (-b + Math.sqrt(delta)) / (2 * a)
-        const x2 = (-b - Math.sqrt(delta)) / (2 * a)
-        console.log(x1)
-        console.log(x2)
-    }
+  // Verifique se o delta é negativo, o que significa que não há raízes reais.
+  if (delta < 0) {
+    console.log("Sem raízes reais");
+  } else {
+    // Calcule as duas raízes usando a fórmula de Bhaskara.
+    const x1 = (-b + Math.sqrt(delta)) / (2 * a);
+    const x2 = (-b - Math.sqrt(delta)) / (2 * a);
+
+    // Imprima as raízes no console.
+    console.log("Raiz 1:", x1);
+    console.log("Raiz 2:", x2);
+  }
 }
 
-console.log(bhaskara(1, -1, -6))
+// Chame a função bhaskara com coeficientes de exemplo (a=1, b=-1, c=-6) e imprima as raízes no console.
+console.log(bhaskara(1, -1, -6));
