da477fe472bf2641a6e256fa962ce401dc0a85bd
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-09-28 00:53:58 -0300

    Exercício 3
    Este programa é uma função simples que realiza um cálculo de exponenciação. Ele usa a função `Math.pow` para calcular a potência da base elevada ao expoente e, em seguida, imprime o resultado no console. O exemplo dado chama a função `exponenciacao` com base 4 e expoente 3, resultando na impressão do valor 64 no console (pois 4^3 = 64).

--- exercicios/3.js
+++ exercicios/3.js
@@ -1,5 +1,9 @@
-function exponenciacao(base, expoente){
-    return Math.pow(base, expoente)
+// Define uma função chamada exponenciacao que recebe dois argumentos: base e expoente.
+function exponenciacao(base, expoente) {
+  // Use a função Math.pow para calcular a potência da base elevada ao expoente.
+  // Isso calcula base^expoente e retorna o resultado.
+  return Math.pow(base, expoente);
 }
 
-console.log(exponenciacao(4,3))
+// Chama a função exponenciacao com valores de exemplo (base = 4, expoente = 3) e imprime o resultado no console.
+console.log(exponenciacao(4, 3));
