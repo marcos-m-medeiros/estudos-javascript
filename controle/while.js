f8ca88fa9b134259b860814a32b448ab23bfb93e
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-09-28 01:38:20 -0300

    while
    Este programa é um exemplo de geração de números aleatórios e uso de loops em JavaScript. Ele continuará gerando números aleatórios dentro do intervalo de -1 a 9 (inclusive) até que o número -1 seja gerado, momento em que ele sairá do loop e imprimirá "Até a próxima!" no console. É uma simulação de um programa de escolha aleatória.

--- controle/while.js
+++ controle/while.js
@@ -1,13 +1,23 @@
+// Define uma função chamada 'getInteiroAleatorioEntre' que gera um número inteiro aleatório entre 'min' (inclusive) e 'max' (exclusivo).
 function getInteiroAleatorioEntre(min, max) {
-    const valor = Math.random() * (max - min) + min
-    return Math.floor(valor)
+  // Gera um número aleatório entre 0 (inclusive) e 1 (exclusivo), multiplica pelo intervalo desejado e adiciona o mínimo para garantir que o número gerado esteja dentro do intervalo desejado.
+  const valor = Math.random() * (max - min) + min;
+
+  // Usa 'Math.floor' para arredondar o número para o inteiro mais próximo e retorna o resultado.
+  return Math.floor(valor);
 }
 
-let opcao = 0
+// Inicializa a variável 'opcao' com o valor 0.
+let opcao = 0;
 
+// Inicia um loop 'while' que continuará até que a 'opcao' seja igual a -1.
 while (opcao != -1) {
-    opcao = getInteiroAleatorioEntre(-1, 10)
-    console.log(`Opçao escolhida foi ${opcao}`)
+  // Chama a função 'getInteiroAleatorioEntre' com os valores -1 (mínimo) e 10 (máximo) para gerar um número aleatório.
+  opcao = getInteiroAleatorioEntre(-1, 10);
+
+  // Imprime no console a opção escolhida.
+  console.log(`Opção escolhida foi ${opcao}`);
 }
 
-console.log('Até a próxima!')
+// Após o loop, imprime "Até a próxima!" no console.
+console.log("Até a próxima!");
