27703d0f6fabb47be2d049800387d1357bcf283d
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-09-28 00:50:31 -0300

    Exercício 5
    Este programa é útil para formatar um valor numérico, como um preço ou quantia em dinheiro, em moeda brasileira. A função `formatar` utiliza a biblioteca `Intl.NumberFormat` para realizar a formatação e, em seguida, o resultado formatado é impresso no console. O exemplo dado chama a função `formatar` com o valor 1231 e imprime o valor formatado em moeda brasileira no console.

--- exercicios/5.js
+++ exercicios/5.js
@@ -1,11 +1,11 @@
-let valor = 1231
+let valor = 1231;
 
-function formatar(valor){
-    let brl = Intl.NumberFormat("BRL", {
-        style: "currency",
-        currency: "BRL"
-    })
-    return brl.format(valor)
+function formatar(valor) {
+  let brl = Intl.NumberFormat("BRL", {
+    style: "currency",
+    currency: "BRL",
+  });
+  return brl.format(valor);
 }
 
-console.log(formatar(valor))
+console.log(formatar(valor));
