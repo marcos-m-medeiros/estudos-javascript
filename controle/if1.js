93f96680ecfa5446b4078eb6f8f3baef29b6c7c9
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-09-28 01:42:07 -0300

    if1
    Este programa ilustra como as condições `if` funcionam com valores booleanos em JavaScript. Se a condição dentro de `if` for verdadeira (avaliada como verdadeira), o bloco de código associado será executado. Caso contrário, o bloco será ignorado. Valores como `null`, `undefined`, `NaN`, uma string vazia `""` e o número zero `0` são avaliados como falsos (falsy), enquanto outros valores são avaliados como verdadeiros (truthy) quando usados em contextos condicionais.

--- controle/if1.js
+++ controle/if1.js
@@ -1,30 +1,30 @@
 function soBoaNoticia(nota) {
-    if (nota >= 7) {
-        console.log('Aprovado com ' + nota)
-    }
+  if (nota >= 7) {
+    console.log("Aprovado com " + nota);
+  }
 }
 
-soBoaNoticia(8.1)
-soBoaNoticia(6.1)
+soBoaNoticia(8.1);
+soBoaNoticia(6.1);
 
 function seForVerdadeEuFalo(valor) {
-    if (valor) {
-        console.log('É verdade... ' + valor)
-    }
+  if (valor) {
+    console.log("É verdade... " + valor);
+  }
 }
 
 //false
-seForVerdadeEuFalo()
-seForVerdadeEuFalo(null)
-seForVerdadeEuFalo(undefined)
-seForVerdadeEuFalo(NaN)
-seForVerdadeEuFalo('')
-seForVerdadeEuFalo(0)
+seForVerdadeEuFalo();
+seForVerdadeEuFalo(null);
+seForVerdadeEuFalo(undefined);
+seForVerdadeEuFalo(NaN);
+seForVerdadeEuFalo("");
+seForVerdadeEuFalo(0);
 
 //true
-seForVerdadeEuFalo(-1)
-seForVerdadeEuFalo(' ')
-seForVerdadeEuFalo('?')
-seForVerdadeEuFalo([])
-seForVerdadeEuFalo([1, 2])
-seForVerdadeEuFalo({})
+seForVerdadeEuFalo(-1);
+seForVerdadeEuFalo(" ");
+seForVerdadeEuFalo("?");
+seForVerdadeEuFalo([]);
+seForVerdadeEuFalo([1, 2]);
+seForVerdadeEuFalo({});
