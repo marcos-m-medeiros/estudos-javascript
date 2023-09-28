e71fa4283e90c7df8cf13433c6ad512827ea3063
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-09-28 00:57:29 -0300

    Exercício 1
    Este programa define uma função `calc` que realiza operações matemáticas básicas e exibe os resultados no console. Você declara três variáveis, mas `res` não é usada no código fornecido. A função `calc` aceita dois parâmetros, realiza quatro operações diferentes e imprime uma mensagem no console que inclui os resultados dessas operações quando chamada com os valores 10 e 10 como argumentos.

--- exercicios/1.js
+++ exercicios/1.js
@@ -1,17 +1,20 @@
-let n1; // Declaração de uma variável n1 (será usada para armazenar o primeiro número).
-let n2; // Declaração de uma variável n2 (será usada para armazenar o segundo número).
-let res; // Declaração de uma variável res (não está sendo utilizada neste código).
+// Declaração de variáveis para armazenar números e um resultado (res).
+let n1; // Variável para o primeiro número (não inicializada aqui).
+let n2; // Variável para o segundo número (não inicializada aqui).
+let res; // Variável para armazenar um resultado (não utilizada neste código).
 
-// Esta é a definição de uma função chamada 'calc' que aceita dois parâmetros, 'n1' e 'n2'.
+// Definição de uma função chamada 'calc' que aceita dois parâmetros: 'n1' e 'n2'.
 function calc(n1, n2) {
-    // Dentro da função, quatro variáveis locais (sum, sub, mul e div) são criadas para armazenar resultados de operações.
-    const sum = n1 + n2; // Calcula a soma de n1 e n2.
-    const sub = n1 - n2; // Calcula a subtração de n1 por n2.
-    const mul = n1 * n2; // Calcula a multiplicação de n1 por n2.
-    const div = n1 / n2; // Calcula a divisão de n1 por n2.
+  // Dentro da função, quatro variáveis locais (sum, sub, mul e div) são criadas para armazenar resultados de operações.
+  const sum = n1 + n2; // Calcula a soma de n1 e n2.
+  const sub = n1 - n2; // Calcula a subtração de n1 por n2.
+  const mul = n1 * n2; // Calcula a multiplicação de n1 por n2.
+  const div = n1 / n2; // Calcula a divisão de n1 por n2.
 
-    // Exibe no console uma mensagem que inclui os resultados das operações.
-    console.log(`Sum: ${sum} Subtraction: ${sub} Multiplication: ${mul} Divided: ${div}`);
+  // Exibe no console uma mensagem que inclui os resultados das operações.
+  console.log(
+    `Sum: ${sum} Subtraction: ${sub} Multiplication: ${mul} Divided: ${div}`
+  );
 }
 
 // Chama a função 'calc' com os valores 10 e 10 como argumentos.
