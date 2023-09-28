0edb94783db96541c711a255421321c737b81cb7
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-09-28 01:33:36 -0300

    ifElse
    Este programa é um exemplo simples de avaliação condicional onde a função `imprimirResultado` é chamada com diferentes valores de nota como argumentos. Dependendo do valor da nota, a função imprime "Aprovado" ou "Reprovado". No entanto, no exemplo 3, onde a nota é uma string não numérica, o comportamento não é previsível, e geralmente você obterá "Reprovado" devido à comparação com `>= 7`.

--- controle/ifElse.js
+++ controle/ifElse.js
@@ -1,11 +1,14 @@
+// Define uma função anônima chamada 'imprimirResultado' que aceita um parâmetro 'nota'.
 const imprimirResultado = function (nota) {
-    if (nota >= 7) {
-        console.log('Aprovado.')
-    } else {
-        console.log('Reprovado.')
-    }
-}
+  // Verifica se a 'nota' é maior ou igual a 7.
+  if (nota >= 7) {
+    console.log("Aprovado."); // Se a condição for verdadeira, imprime "Aprovado".
+  } else {
+    console.log("Reprovado."); // Se a condição for falsa, imprime "Reprovado".
+  }
+};
 
-imprimirResultado(10)
-imprimirResultado(4)
-imprimirResultado('TESTE')
+// Chama a função 'imprimirResultado' com diferentes valores de nota como argumentos.
+imprimirResultado(10); // Exemplo 1: "Aprovado" será impresso no console.
+imprimirResultado(4); // Exemplo 2: "Reprovado" será impresso no console.
+imprimirResultado("TESTE"); // Exemplo 3: Como "TESTE" não é um número válido, a saída não será previsível.
