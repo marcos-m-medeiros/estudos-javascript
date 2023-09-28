eca12ac2aa8e4593b94a2704083f19f17feff414
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-09-28 00:52:45 -0300

    Exercício 4
    Este programa é uma calculadora simples que calcula a divisão e o resto da divisão entre dois números e retorna os resultados em um array. O exemplo dado chama a função `res` com os valores 30 e 4 e imprime o resultado no console, que será um array com duas strings, uma para a divisão e outra para o resto.

--- exercicios/4.js
+++ exercicios/4.js
@@ -1,9 +1,17 @@
+// Define uma função chamada res que recebe dois números (n1 e n2) como argumentos.
 function res(n1, n2) {
-    let divisao = n1 / n2
-    let resto = n1 % n2
-    let resultado = ['Divisão: ' + divisao , 'Resto: ' + resto]
+  // Calcula a divisão dos números e armazena o resultado em uma variável chamada "divisao".
+  let divisao = n1 / n2;
 
-    return resultado
+  // Calcula o resto da divisão dos números e armazena o resultado em uma variável chamada "resto".
+  let resto = n1 % n2;
+
+  // Cria um array chamado "resultado" que contém duas strings formatadas com os resultados da divisão e do resto.
+  let resultado = ["Divisão: " + divisao, "Resto: " + resto];
+
+  // Retorna o array "resultado" que contém as informações da divisão e do resto.
+  return resultado;
 }
 
-console.log(res(30,4))
+// Chama a função res com valores de exemplo (n1 = 30, n2 = 4) e imprime o resultado no console.
+console.log(res(30, 4));
