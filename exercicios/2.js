1e11dfc5e4824121bf183aa2727cb8e50415957a
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-09-28 00:55:36 -0300

    Exercício 2
    Este programa é uma maneira simples de determinar o tipo de triângulo com base em seus lados. Ele usa condicionais `if` e `else if` para verificar se o triângulo é equilátero (todos os lados iguais), isósceles (pelo menos dois lados iguais) ou escaleno (nenhum dos lados iguais). O exemplo dado chama a função `triangle` com três lados iguais a 1 e imprime "Equilátero" no console.

--- exercicios/2.js
+++ exercicios/2.js
@@ -1,11 +1,25 @@
+// Define uma função chamada triangle que recebe três números (n1, n2 e n3) representando os lados de um triângulo.
 function triangle(n1, n2, n3) {
-    if (n1 == n2 && n1 == n3 && n2 == n1 && n2 == n3 && n3 == n1 && n3 == n2) {
-        console.log('Equilátero')
-    } else if (n1 == n2 || n1 == n3 || n2 == n1 || n2 == n3 || n3 == n1 || n3 == n2) {
-        console.log('Isósceles')
-    } else {
-        console.log('Escaleno')
-    }
+  // Verifica se todos os lados são iguais, o que significa que o triângulo é equilátero.
+  if (n1 == n2 && n1 == n3 && n2 == n1 && n2 == n3 && n3 == n1 && n3 == n2) {
+    console.log("Equilátero"); // Se todos os lados forem iguais, imprime "Equilátero".
+  }
+  // Verifica se pelo menos dois lados são iguais, o que significa que o triângulo é isósceles.
+  else if (
+    n1 == n2 ||
+    n1 == n3 ||
+    n2 == n1 ||
+    n2 == n3 ||
+    n3 == n1 ||
+    n3 == n2
+  ) {
+    console.log("Isósceles"); // Se pelo menos dois lados forem iguais, imprime "Isósceles".
+  }
+  // Se nenhum dos casos anteriores for verdadeiro, o triângulo é escaleno.
+  else {
+    console.log("Escaleno"); // Se nenhum dos lados for igual, imprime "Escaleno".
+  }
 }
 
-triangle(1,1,1)
+// Chama a função triangle com valores de exemplo (todos iguais a 1) para determinar o tipo de triângulo.
+triangle(1, 1, 1);
