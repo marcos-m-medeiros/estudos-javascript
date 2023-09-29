5852346ee4960b62dbab31bdee9aabe5d3d8cf1d
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-09-29 19:31:26 -0300

    factory1
    Neste código, a função `criarPessoa` é uma fábrica simples que retorna um objeto com propriedades `nome` e `sobrenome`. Quando você chama `criarPessoa()`, um novo objeto é criado e retornado, e esse objeto é impresso no console. O resultado será um objeto com os valores definidos para `nome` e `sobrenome`, que, neste caso, são "Ana" e "Silva".

---
+++ funcao/factory1.js
@@ -0,0 +1,13 @@
+// Definindo uma função chamada 'criarPessoa'
+function criarPessoa() {
+  // Dentro da função, um objeto literal é criado e retornado
+  return {
+    // Definindo propriedades do objeto
+    nome: "Ana",
+    sobrenome: "Silva",
+  };
+}
+
+// Chamando a função 'criarPessoa' para criar um objeto que representa uma pessoa
+// e imprimindo o resultado no console
+console.log(criarPessoa());
