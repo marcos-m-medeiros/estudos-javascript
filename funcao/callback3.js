dbdccbcfe22b7ef006c9d70ba6e8e9008fcd38bf
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-09-29 15:02:21 -0300

    callback3
    Neste código, está sendo selecionando o elemento `<body>` do documento HTML e atribuindo a ele um evento de clique (`onclick`). Quando o corpo da página é clicado, a função anônima (callback) dentro do evento é chamada, e ela registra a mensagem "O evento ocorreu!" no console do navegador. Isso é um exemplo de como usar callbacks para responder a eventos em JavaScript.

---
+++ funcao/callback3.js
@@ -0,0 +1,7 @@
+// Seleciona o elemento <body> do documento HTML
+document.getElementsByTagName("body")[0].onclick = function (e) {
+  // Esta função será executada quando ocorrer um clique no corpo da página
+
+  // Registra uma mensagem no console quando o evento ocorre
+  console.log("O evento ocorreu!");
+};
