cace6bc8ec7e2b075882f640ea53f4e8a1ed47a5
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-10-01 14:25:01 -0300

    oo
    Exemplos de Orientação à Objetos e seus quatro princípios importantes

---
+++ objeto/oo.js
@@ -0,0 +1,24 @@
+// CÓDIGO NÃO EXECUTÁVEL
+
+// Procedural
+processamento(valor1, valor2, valor3); // Chamada de função procedural.
+
+// OO
+objeto = {
+  valor1,
+  valor2,
+  valor3,
+  processamento() {
+    // Método associado ao objeto.
+    // Foco passou a ser o objeto.
+    // Isso demonstra Encapsulamento e Abstração, pois os dados (valor1, valor2, valor3) e o comportamento (processamento) estão encapsulados no objeto.
+  },
+};
+
+objeto.processamento(); // Chamada do método 'processamento' do objeto.
+
+// Princípios importantes
+// 1. Abstração: A abstração é alcançada ao encapsular dados e comportamentos dentro do objeto 'objeto', expondo apenas a interface necessária (método 'processamento').
+// 2. Encapsulamento: O encapsulamento ocorre ao agrupar os dados (valor1, valor2, valor3) e o comportamento (processamento) dentro do objeto, ocultando detalhes internos.
+// 3. Herança (prototype): A herança envolve a criação de classes e a herança de propriedades/métodos de uma classe para outra.
+// 4. Polimorfismo: O polimorfismo envolve a capacidade de objetos de classes diferentes responderem ao mesmo método de maneira diferente.
