4a0a643583c8f682a31cb7ab990216a6f0191e4f
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-10-02 21:55:15 -0300

    24
    Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while.

--- exercicios/24.js
+++ exercicios/24.js
@@ -1,13 +1,13 @@
-// Inicialize a variável 'txt' com o valor 0
-let txt = 0;
+// Inicialize a variável 'txt' com o valor 1
+let txt = 1;
 
-// Inicia um loop while que continuará enquanto 'txt' for menor ou igual a 10
-while (txt <= 10) {
+// Inicia um loop while que continuará enquanto 'txt' for menor ou igual a 11
+while (txt <= 11) {
   // Imprime a mensagem "Hello World!" no console a cada iteração do loop
-  console.log("Hello World!");
+  console.log("Hello World! " + txt);
 
   // Incrementa a variável 'txt' em 1 a cada iteração
   txt++;
 }
 
-// O loop continuará até que 'txt' seja maior que 10, momento em que ele será encerrado
+// O loop continuará até que 'txt' seja maior que 11, momento em que ele será encerrado
