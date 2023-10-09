7fa2265a23a97a89fc4d6ffcd9f242384988aa3d
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-10-09 17:36:38 -0300

    concat
    Basicamente, este código cria duas arrays, `filhas` e `filhos`, e depois as concatena em uma terceira array chamada `todos` usando o método `concat()`. Em seguida, ele exibe as três arrays no console. Além disso, o segundo `console.log()` concatena várias arrays e valores diferentes em uma única array vazia e a exibe no console.

---
+++ array/concat.js
@@ -0,0 +1,12 @@
+// Definindo duas arrays com nomes de filhas e filhos
+const filhas = ["Ualeskah", "Cibelena"];
+const filhos = ["Uoxiton", "Uesclei"];
+
+// Concatenando as duas arrays em uma terceira chamada 'todos'
+const todos = filhas.concat(filhos);
+
+// Exibindo as três arrays no console
+console.log(todos, filhas, filhos);
+
+// Concatenando várias arrays e valores em uma única array e exibindo no console
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]));
